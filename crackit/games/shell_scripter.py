9f4a8ec6e3845b16133a03a3b8a118899fb25ca6
Wren Powell <wrenp@duck.com>
2018-07-24 14:21:22 -0400

    Add support for multiple positional arguments
    Previously, the order of positional/required arguments was undefined,
    meaning that no more than one positional argument could be used in a
    command.

--- crackit/games/shell_scripter.py
+++ crackit/games/shell_scripter.py
@@ -26,11 +26,8 @@ from typing import List
 
 from crackit.utils import clear_line, format_banner
 
-# The string that is printed before each line of user input.
-INPUT_PROMPT = "$ "
-
-# The string that is printed before each line of output.
-OUTPUT_PROMPT = "$ "
+# The string that is printed before each command and line of user input.
+COMMAND_PROMPT = "$ "
 
 # All the characters that can be used to separate flags from their arguments.
 FLAG_SEPARATORS = [" ", "="]
@@ -55,18 +52,32 @@ INPUT_FILE_NAMES = [
     "input.txt", "input", "input_file.txt", "input_file", "source.txt", "source", "data.txt", "data",
 ]
 
+#
+# The following constants are commonly used arguments for commands.
+#
+
 # Directory paths to be used as arguments in commands.
-MISC_DIR_PATHS = [
+ARG_DIR_PATHS = [
     "~/Documents", "/home/lostatc/Documents", "~/Downloads", "/home/lostatc/Downloads", "~/Music",
     "/home/lostatc/Music", "~/Pictures", "/home/lostatc/Pictures", "~/Videos", "/home/lostatc/Vidoes", ".", "/", "/dev",
     "/etc", "/home/lostatc", "/mnt", "/proc", "/run", "/run/media/lostatc", "/sys", "/tmp", "/usr/share",
     "/usr/local/share", "/var", "/var/log",
 ]
 
-# Shell globing patterns to be used as arguments in commands.
-MISC_GLOB_PATTERNS = [
-    "\"*.png\"", "\"*.jpg\"", "\"*.mp3\"", "\"*.flac\"", "\"*.mp4\"", "\"*.log\"", "\"backup.tar.??\"",
-    "\"Document.od[tspgf]\"", "\"/dev/sd[ab]*\"", "\"/proc/[0-9]*\"",
+# Shell globing patterns for matching file names to be used as arguments in commands.
+ARG_FILE_GLOB_PATTERNS = [
+    "\"*.png\"", "\"*.jpg\"", "\"*.mp3\"", "\"*.flac\"", "\"*.mp4\"", "\"*.log\"", "\"*.tar.*\"",
+    "\"*.od[tspgf]\"", "\".*\"", "\"*.doc[xm]\"", "\"*.xls[xm]\"",
+]
+
+# Number ranges to be used as arguments in the `cut` command.
+ARG_CUT_RANGES = [
+    "1", "\"-10\"", "\"3-\"", "\"2-4\"", "\"1-2\"", "\"5-\"", "\"-20\"",
+]
+
+# Delimiters to be used as arguments in the `cut` command.
+ARG_CUT_DELIMITERS = [
+    "\" \"", "\",\"", "\"|\"", "\"\\n\"", "\"\\0\"",
 ]
 
 
@@ -75,15 +86,11 @@ class Argument:
 
     Attributes:
         names: All the names that the command can have.
-        possible_args: A list of possible arguments for the command.
-        required: The argument must always appear.
+        args: A list of possible arguments for the command.
     """
-    def __init__(
-            self, names: List[str], possible_args: List[str],
-            required: bool = False) -> None:
+    def __init__(self, names: List[str], args: List[str]) -> None:
         self.names = names
-        self.possible_args = possible_args
-        self.required = required
+        self.args = args
 
     def get_random(self, flag_separator: str) -> str:
         """Generate a random argument within the given constraints as a string.
@@ -94,7 +101,7 @@ class Argument:
         Returns:
             The argument as a string.
         """
-        return flag_separator.join(random.choice(choices) for choices in (self.names, self.possible_args) if choices)
+        return flag_separator.join(random.choice(choices) for choices in (self.names, self.args) if choices)
 
 
 # TODO: Accept multiple lists for possible_args to account for mutually-exclusive arguments.
@@ -103,15 +110,19 @@ class Command:
 
     Attributes:
         name: The name of the command.
-        possible_args: A list of possible arguments for the command.
+        positional_args: A list of required positional arguments for the command. Each command in this list is used once
+            in the order that it appears.
+        optional_args: A list of optional arguments for the command. Commands in this list are chosen randomly and can
+            be used in any order.
         redirect_output: The command can redirect its output.
         redirect_input: The command must redirect its input.
     """
     def __init__(
-            self, name: str, possible_args: List[Argument],
+            self, name: str, positional_args: List[Argument], optional_args: List[Argument],
             redirect_output: bool = False, redirect_input: bool = False) -> None:
         self.name = name
-        self.possible_args = possible_args
+        self.positional_args = positional_args
+        self.optional_args = optional_args
         self.redirect_output = redirect_output
         self.redirect_input = redirect_input
 
@@ -125,30 +136,22 @@ class Command:
         Returns:
             The command as a string.
         """
-        selected_args = []
-        remaining_args = []
+        selected_args = self.positional_args.copy()
+        remaining_args = self.optional_args.copy()
 
-        # Add all required arguments and ensure that they aren't added again.
-        for argument in self.possible_args:
-            if argument.required:
-                selected_args.append(argument)
-            else:
-                remaining_args.append(argument)
-
+        # Select random parameters.
         flag_separator = random.choice(FLAG_SEPARATORS)
         number_of_args = random.randrange(MIN_ARGS, MAX_ARGS)
 
-        # Ensure that arguments are added randomly.
+        # Add a random number of optional arguments.
         random.shuffle(remaining_args)
-
-        # Add a random number of arguments.
         for _ in range(number_of_args):
             try:
                 selected_args.append(remaining_args.pop())
             except IndexError:
                 break
 
-        # Generate a random command string.
+        # Generate a command string.
         if not selected_args:
             command_string = self.name
         else:
@@ -208,8 +211,8 @@ COMMANDS = (
                     "\"^/\w(:/\w)*$\"", "\"([r-][w-][x-]){3}\"", "Error", "Exception", "Warning", "Info", "NULL",
                     "true", "false",
                 ],
-                required=True,
             ),
+        ], [
             Argument(["-E", "--extended-regexp", "-G", "--basic-regexp", "-F", "--fixed-string"], []),
             Argument(["-i", "--ignore-case"], []),
             Argument(["-v", "--invert-match"], []),
@@ -223,7 +226,8 @@ COMMANDS = (
     ),
     Command(
         "find", [
-            Argument([], MISC_DIR_PATHS, required=True),
+            Argument([], ARG_DIR_PATHS),
+        ], [
             Argument(["-depth"], []),
             Argument(["-maxdepth"], ["0", "1", "2", "3", "4", "5"]),
             Argument(["-mindepth"], ["0", "1", "2", "3", "4", "5"]),
@@ -235,7 +239,7 @@ COMMANDS = (
             Argument(["-group"], ["root", "lostatc", "wheel", "nobody", "users"]),
             Argument(["-links"], ["0", "1", "2", "3", "4", "5"]),
             Argument(["-mmin"], ["1", "5", "10", "15", "20", "25", "30"]),
-            Argument(["-name"], MISC_GLOB_PATTERNS),
+            Argument(["-name"], ARG_FILE_GLOB_PATTERNS),
             Argument(["-perm"], ["\"/a+w\"", "\"-g+w\"", "\"u=w\"", "\"-a+r\"", "\"/a+x\"", "\"-220\""]),
             Argument(["-size"], ["100k", "120K", "50M", "200M", "1G", "10G"]),
             Argument(["-delete"], []),
@@ -247,10 +251,11 @@ COMMANDS = (
     ),
     Command(
         "ls", [
-            Argument([], MISC_DIR_PATHS, required=True),
+            Argument([], ARG_DIR_PATHS),
+        ], [
             Argument(["-a", "--all"], []),
             Argument(["-d", "--directory"], []),
-            Argument(["--hide"], MISC_GLOB_PATTERNS),
+            Argument(["--hide"], ARG_FILE_GLOB_PATTERNS),
             Argument(["-l"], []),
             Argument(["-N", "--literal"], []),
             Argument(["-Q", "--quote-name"], []),
@@ -264,14 +269,14 @@ COMMANDS = (
         redirect_output=True,
     ),
     Command(
-        "cut", [
-            Argument(["-b", "--bytes"], ["1", "-10", "3-", "2-4", "1-2", "5-", "-20"]),
-            Argument(["-c", "--characters"], ["1", "-10", "3-", "2-4", "1-2", "5-", "-20"]),
-            Argument(["-d", "--delimiter"], ["\" \"", "\",\"", "\"|\"", "\"\\n\"", "\"\\0\""]),
-            Argument(["-f", "--fields"], ["1", "-10", "3-", "2-4", "1-2", "5-", "-20"]),
+        "cut", [], [
+            Argument(["-b", "--bytes"], ARG_CUT_RANGES),
+            Argument(["-c", "--characters"], ARG_CUT_RANGES),
+            Argument(["-d", "--delimiter"], ARG_CUT_DELIMITERS),
+            Argument(["-f", "--fields"], ARG_CUT_RANGES),
             Argument(["--complement"], []),
             Argument(["-s", "--only-delimited"], []),
-            Argument(["--output-delimiter"], ["\" \"", "\",\"", "\"|\"", "\"\\n\"", "\"\\0\""]),
+            Argument(["--output-delimiter"], ARG_CUT_DELIMITERS),
         ],
         redirect_input=True,
         redirect_output=True,
@@ -285,14 +290,12 @@ def main(commands_to_win: int = 10) -> None:
     Args:
         commands_to_win: The number of commands that must be correctly entered to win the game.
     """
-    # for _ in range(5):
-    #     print(random.choice(COMMANDS).get_random())
     for _ in range(commands_to_win):
         command_string = random.choice(COMMANDS).get_random()
-        print(OUTPUT_PROMPT + command_string)
+        print(COMMAND_PROMPT + command_string)
 
         while True:
-            if input(INPUT_PROMPT) == command_string:
+            if input(COMMAND_PROMPT) == command_string:
                 break
             else:
                 clear_line()
