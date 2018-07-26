be47d8066f418e07ffcf4ac8cac6fbacf3834d92
Wren Powell <wrenp@duck.com>
2018-07-26 11:38:46 -0400

    Change difficulty presets

--- crackit/difficulty.py
+++ crackit/difficulty.py
@@ -1,7 +1,6 @@
 import enum
 
-from crackit.games import hash_cracker
-from crackit.games import shell_scripter
+from crackit.games import hash_cracker, shell_scripter
 
 
 class Difficulty(enum.Enum):
@@ -10,7 +9,7 @@ class Difficulty(enum.Enum):
     HARD = 3
 
 
-def start_hash_cracker(difficulty: Difficulty):
+def start_hash_cracker(difficulty: Difficulty) -> None:
     """Start the game "hash_cracker" with a given difficulty."""
     if difficulty is Difficulty.EASY:
         hash_cracker.main(rows_to_win=8, starting_rows=4, columns=6)
@@ -20,11 +19,12 @@ def start_hash_cracker(difficulty: Difficulty):
         hash_cracker.main(rows_to_win=8, starting_rows=4, columns=10)
 
 
-def start_shell_scripter(difficulty: Difficulty):
+def start_shell_scripter(difficulty: Difficulty) -> None:
     """Start the game "shell_scripter" with a given difficulty."""
+    # With these settings, the average number of characters per command increases linearly with each difficulty level.
     if difficulty is Difficulty.EASY:
-        shell_scripter.main(commands_to_win=15, min_args=0, max_args=3, redirect_probability=0.10)
+        shell_scripter.main(commands_to_win=15, min_args=0, max_args=3, redirect_probability=0.1, pipe_probability=0.2)
     if difficulty is Difficulty.NORMAL:
-        shell_scripter.main(commands_to_win=15, min_args=0, max_args=5, redirect_probability=0.25)
+        shell_scripter.main(commands_to_win=15, min_args=1, max_args=4, redirect_probability=0.3, pipe_probability=0.4)
     if difficulty is Difficulty.HARD:
-        shell_scripter.main(commands_to_win=15, min_args=0, max_args=7, redirect_probability=0.40)
+        shell_scripter.main(commands_to_win=15, min_args=2, max_args=5, redirect_probability=0.4, pipe_probability=0.5)
