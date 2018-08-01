a3117409a666c3c92c6997a60f667e07b01a691e
Wren Powell <wrenp@duck.com>
2018-08-01 09:04:02 -0400

    Remove duplicate code

--- crackit/launcher/cli.py
+++ crackit/launcher/cli.py
@@ -21,10 +21,26 @@ import click
 from prompt_toolkit import print_formatted_text
 
 from crackit.launcher import gui
-from crackit.launcher.common import GAMES, Difficulty
+from crackit.launcher.common import Game, GAMES, Difficulty
 
 
-@click.group(name="crackit", invoke_without_command=True)
+def _get_game(name: str) -> Game:
+    """Get a game from its name."""
+    try:
+        return [item for item in GAMES if item.name == name][0]
+    except IndexError:
+        raise click.BadParameter("'{0}'".format(name))
+
+
+def _get_difficulty(name: str) -> Difficulty:
+    """Get a difficulty from its name."""
+    try:
+        return [item for item in Difficulty if item.value.lower() == name.lower()][0]
+    except IndexError:
+        raise click.BadParameter("'{0}'".format(name))
+
+
+@click.group(invoke_without_command=True)
 @click.pass_context
 def cli(ctx):
     # If the command is run without any arguments, options or subcommands, run the GUI launcher.
@@ -40,26 +56,11 @@ def cli(ctx):
 )
 def play(game: str, difficulty: str):
     """Play the game named GAME."""
-    try:
-        selected_game = [item for item in GAMES if item.name == game][0]
-    except IndexError:
-        raise click.BadParameter("'{0}'".format(game))
-
-    try:
-        selected_difficulty = [item for item in Difficulty if item.value.lower() == difficulty.lower()][0]
-    except IndexError:
-        raise click.BadParameter("'{0}'".format(difficulty))
-
-    selected_game.launcher(selected_difficulty)
+    _get_game(game).launcher(_get_difficulty(difficulty))
 
 
 @cli.command(name="get-description", short_help="Get the description of a game.")
 @click.argument("game", type=str)
 def get_description(game: str):
     """Get the description of the game named GAME."""
-    try:
-        selected_game = [item for item in GAMES if item.name == game][0]
-    except IndexError:
-        raise click.BadParameter("'{0}'".format(game))
-
-    print_formatted_text(selected_game.description)
+    print_formatted_text(_get_game(game).description)
