9076a10c32fab386b3afa379bee74c937a0966b3
Wren Powell <wrenp@duck.com>
2018-08-07 22:38:33 -0400

    Undo temporary change in difficulty parameters

--- crackit/launcher/common.py
+++ crackit/launcher/common.py
@@ -57,7 +57,7 @@ TimerFunc = Callable[[Difficulty], float]
 def _start_hash_cracker(difficulty: Difficulty) -> None:
     """Start the game "hash_cracker" with a given difficulty."""
     if difficulty is Difficulty.EASY:
-        hash_cracker.main(rows_to_win=5, starting_rows=4, columns=6)
+        hash_cracker.main(rows_to_win=8, starting_rows=4, columns=6)
     if difficulty is Difficulty.NORMAL:
         hash_cracker.main(rows_to_win=8, starting_rows=4, columns=8)
     if difficulty is Difficulty.HARD:
@@ -78,7 +78,7 @@ def _start_shell_scripter(difficulty: Difficulty) -> None:
     """Start the game "shell_scripter" with a given difficulty."""
     # With these settings, the average number of characters per command increases linearly with each difficulty level.
     if difficulty is Difficulty.EASY:
-        shell_scripter.main(commands_to_win=1, min_args=0, max_args=3, redirect_probability=0.1, pipe_probability=0.2)
+        shell_scripter.main(commands_to_win=15, min_args=0, max_args=3, redirect_probability=0.1, pipe_probability=0.2)
     if difficulty is Difficulty.NORMAL:
         shell_scripter.main(commands_to_win=15, min_args=1, max_args=4, redirect_probability=0.3, pipe_probability=0.4)
     if difficulty is Difficulty.HARD:
