9b23eb749558b03e5bac84033b49a3fa5992ad00
Wren Powell <wrenp@duck.com>
2018-08-09 18:48:16 -0400

    Update documentation

--- crackit/games/__init__.py
+++ crackit/games/__init__.py
@@ -1,11 +1,12 @@
 """Games to play in the terminal.
 
 Games in this package should have a main function that accepts parameters affecting how the game is played. This main
-function must return once the game is complete. For each game added to this package, you must do two things:
+function must return once the game is complete. For each game added to this package, you must do the following:
 
-1) You must create a corresponding text file in `crackit/descriptions/` containing a description of the game including
-how to play it. The contents of this file must be wrapped to 80 characters.
+1) Create a corresponding Markdown file in `crackit/descriptions/` containing a description of the game including how to
+play it. The contents of this file should be wrapped to 80 characters.
 
-2) You must add an entry for the game in `crackit/launcher/common.py` including a function which defines difficulty
-presets.
+2) Update README.md with a link to the description of the game.
+
+3) Add an entry for the game in `crackit/launcher/common.py`.
 """
